96cc79fc3295e6643033e2f05ddf2fc4e1d6d276
Anooptej Thotapalli <anoop@opsmx.com>
2022-01-17 18:46:38 +0530

    Update Clouddriver-DB-Sql.sql

--- Create-DB-Files/Clouddriver-DB-Sql.sql
+++ Create-DB-Files/Clouddriver-DB-Sql.sql
@@ -3,5 +3,5 @@
 CREATE SCHEMA `clouddriver` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER 'clouddriver_service' IDENTIFIED BY 'OpsMx2021';
 CREATE USER 'clouddriver_migrate' IDENTIFIED BY 'OpsMx2021';
-GRANT  SELECT, INSERT, UPDATE, CREATE, DELETE, EXECUTE, SHOW VIEW ON `cloud driver`.*   TO 'clouddriver_service';
+GRANT  SELECT, INSERT, UPDATE, CREATE, DELETE, EXECUTE, SHOW VIEW ON `clouddriver`.*   TO 'clouddriver_service';
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, LOCK TABLES, EXECUTE, SHOW VIEW  ON `clouddriver`.* TO 'clouddriver_migrate';
