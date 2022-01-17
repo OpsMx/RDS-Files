4872747ab61d4c05fa64889d9dc0344ba31ac218
Anooptej Thotapalli <anoop@opsmx.com>
2022-01-17 18:24:27 +0530

    Update Orca-DB-Sql.sql

--- Create-DB-Files/Orca-DB-Sql.sql
+++ Create-DB-Files/Orca-DB-Sql.sql
@@ -1,7 +1,7 @@
 -- Note: No Special Characters for Password
 
 CREATE SCHEMA `orca` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER 'orca_service' IDENTIFIED BY 'orcapasswd';
-CREATE USER 'orca_migrate' IDENTIFIED BY 'orcapasswd';
+CREATE USER 'orca_service' IDENTIFIED BY 'OpsMx2022';
+CREATE USER 'orca_migrate' IDENTIFIED BY 'OpsMx2022';
 GRANT  SELECT, INSERT, UPDATE, DELETE, EXECUTE, SHOW VIEW ON `orca`.*   TO 'orca_service';
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, LOCK TABLES, EXECUTE, SHOW VIEW  ON `orca`.* TO 'orca_migrate';
