9650a2a87fa4e461ffd51351179baef5c838b32b
Anooptej Thotapalli <anoop@opsmx.com>
2022-10-29 00:38:45 +0530

    Update Orca-DB-Sql.sql

--- Create-DB-Files/Orca-DB-Sql.sql
+++ Create-DB-Files/Orca-DB-Sql.sql
@@ -1,7 +1,15 @@
 -- Note: No Special Characters for Password
 
-CREATE SCHEMA `orca` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER 'orca_service' IDENTIFIED BY 'OpsMx2022';
-CREATE USER 'orca_migrate' IDENTIFIED BY 'OpsMx2022';
-GRANT  SELECT, INSERT, UPDATE, DELETE, EXECUTE, SHOW VIEW ON `orca`.*   TO 'orca_service';
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, LOCK TABLES, EXECUTE, SHOW VIEW  ON `orca`.* TO 'orca_migrate';
+CREATE SCHEMA `orca` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci; 
+          
+CREATE USER 'orca_service'@'%'IDENTIFIED BY 'OpsMx2021';	
+CREATE USER 'orca_migrate'@'%'IDENTIFIED BY 'OpsMx2021';	
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, EXECUTE, SHOW VIEW ON `orca`.*   TO 'orca_service'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, LOCK TABLES, EXECUTE, SHOW VIEW ON `orca`.* TO `orca_migrate`@'%';
+GRANT ALL PRIVILEGES ON orca.* TO 'orca_migrate'@'%';
+GRANT ALL PRIVILEGES ON orca.* TO 'orca_service'@'%';
+GRANT ALL PRIVILEGES ON orca.* TO 'orca'@'%';
+GRANT ALL ON orca.* TO 'orca'@'%' IDENTIFIED BY 'OpsMx2021' WITH GRANT OPTION;
+GRANT ALL ON orca.* TO 'orca_migrate'@'%' IDENTIFIED BY 'OpsMx2021' WITH GRANT OPTION;
+GRANT ALL ON orca.* TO 'orca_service'@'%' IDENTIFIED BY 'OpsMx2021' WITH GRANT OPTION;
+flush privileges;
